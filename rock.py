81bf20d6a9294a275f83cf1e1a8affc3f6a5a4cd
Runa-w <runa_w@gmx.com>
2022-03-24 11:05:22 -0500

    Added second phase of game and user comments

--- rock.py
+++ rock.py
@@ -1,6 +1,10 @@
-# the following code was created in the first lesson
+# setup for game by importing the random module and declaring the winner variable
 import random
 
+winner = ''
+
+# Here the computer randomly chooses a number between 0 and 2, and each of the three
+# numbers is assigned to a string
 random_choice = random.randint(0, 2)
 
 if random_choice == 0:
@@ -10,7 +14,26 @@ elif random_choice == 1:
 else:
     computer_choice = 'scissors'
 
+# Here an imput is created to ask the user for their choice
+user_choice = input('rock, paper, or scissors? ')
 
-user_choise = input('rock, paper, or scissors? ')
-print('You chose', user_choise, 'and the computer chose', computer_choice)
-# the following code was created after a break
+# Here is the logic of the game, where the computer checks to see if the computer won
+# and makes the proper change to the winner variable
+
+if computer_choice == user_choice:
+    winner = 'Tie'
+elif computer_choice == 'paper' and user_choice == 'rock':
+    winner = 'Computer'
+elif computer_choice == 'rock' and user_choice == 'scissors':
+    winner = 'Computer'
+elif computer_choice == 'scissors' and user_choice == 'paper':
+    winner = 'Computer'
+else:
+    winner = 'User'
+
+# Here the winner of the game is announced, along with the computer's choice
+
+if winner == 'Tie':
+    print('We both chose', computer_choice + ', play again.')
+else:
+    print(winner, 'won. The computer chose', computer_choice + '.')
